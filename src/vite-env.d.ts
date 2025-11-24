33fb2a8298ac4b42bd4e0db5e006afd21e3f080b
caja2 <caja2@local>
2025-11-24 15:05:53 -0300

    🧰 chore(types): remove duplicate '*.svg?react' declaration (use plugin types)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,12 +7,3 @@ declare module '*.svg' {
 		React.SVGProps<SVGSVGElement> & { title?: string }
 	>;
 }
-
-// Soporte para import con query `?react` usado por `vite-plugin-svgr`
-declare module '*.svg?react' {
-	import * as React from 'react'
-	const ReactComponent: React.FunctionComponent<
-		React.SVGProps<SVGSVGElement> & { title?: string }
-	>
-	export default ReactComponent
-}
