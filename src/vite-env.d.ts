ed31c3d997a1f24e01f3e69a503499cf4adbe728
caja2 <caja2@local>
2025-11-24 15:04:29 -0300

    🐛 fix(types): declarar módulos '*.svg?react' para svgr

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,3 +7,12 @@ declare module '*.svg' {
 		React.SVGProps<SVGSVGElement> & { title?: string }
 	>;
 }
+
+// Soporte para import con query `?react` usado por `vite-plugin-svgr`
+declare module '*.svg?react' {
+	import * as React from 'react'
+	const ReactComponent: React.FunctionComponent<
+		React.SVGProps<SVGSVGElement> & { title?: string }
+	>
+	export default ReactComponent
+}
