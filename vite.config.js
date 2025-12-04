ee8c94269f254a687e6edbc766d2a49fffa2f4fb
caja2 <caja2@local>
2025-12-04 15:50:20 -0300

    🔧: update dependencies and build configuration for better performance

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [['babel-plugin-react-compiler']],
+	plugins: [react(), svgr(), tailwindcss()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ['react', 'react-dom'],
+					vendor: ['prop-types'],
+				},
 			},
-		}),
-		svgr(),
-		tailwindcss(),
-	],
+		},
+	},
 });
